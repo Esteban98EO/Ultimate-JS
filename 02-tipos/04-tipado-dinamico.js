8f75ce10e6908956c8b3370e5dd51e515ebf35f0
Esteban Escorcia <estebanfucho98@gmail.com>
2025-11-21 23:51:24 -0800

    Tipado dinamico

---
+++ 02-tipos/04-tipado-dinamico.js
@@ -0,0 +1,11 @@
+let numero = 42; // Number
+let nombre = "Hola Mundo"; // String
+let verdadero = true; // Boolean
+let undef; // Undefined
+let nula = null; // Null
+
+console.log(typeof numero);
+console.log(typeof nombre);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nula);
