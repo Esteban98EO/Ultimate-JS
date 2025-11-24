8f2a6f794f76d3f95229ae69cfda303de6a57c0e
Esteban Escorcia <estebanfucho98@gmail.com>
2025-11-23 22:51:35 -0800

    Aprendemos de como comentar el codigo

---
+++ 02-tipos/05-comentarios.js
@@ -0,0 +1,12 @@
+// Esto es un comentario de una sola línea
+
+/*
+  Esto es un comentario
+  de múltiples líneas
+*/  
+/**
+ * Esto es un comentario de documentación
+ */
+let numero =42; 
+
+console.log(numero);
