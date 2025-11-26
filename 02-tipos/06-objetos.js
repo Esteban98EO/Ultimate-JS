5a153affc7561bdd87c57f0c05f484c4d49291e9
Esteban Escorcia <estebanfucho98@gmail.com>
2025-11-25 16:02:13 -0800

    Jugamos con los objetos para aprender de sus estados y eliminamos un index demas

--- 02-tipos/06-objetos.js
+++ 02-tipos/06-objetos.js
@@ -5,5 +5,22 @@ let edad = 17;
 let esNinja = true;
 
 let personaje = {
-    
+    nombre: "Naruto Uzumaki",
+    anime: "Naruto",
+    edad: 17,
+    esNinja: true
 };
+//Accediendo a las propiedades del objeto
+console.log(personaje);
+console.log("Nombre:", personaje.nombre);
+console.log(personaje.anime);
+console.log(personaje["edad"]);
+
+personaje.edad = 18; //Modificando una propiedad
+console.log("Edad modificada:", personaje.edad);
+
+personaje ["nombre"] = "Sasuke Uchiha"; //Modificando usando notación de corchetes
+console.log("Nombre modificado:", personaje.nombre);
+
+delete personaje.anime; //Eliminando una propiedad
+console.log("Después de eliminar anime:", personaje);
