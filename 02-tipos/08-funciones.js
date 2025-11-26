5c9dcb1a7dc0b42e00ee26e39885df399613edd9
Esteban Escorcia <estebanfucho98@gmail.com>
2025-11-25 17:10:58 -0800

    Vemos funciones pero muy basicas

---
+++ 02-tipos/08-funciones.js
@@ -0,0 +1,10 @@
+function saludar () {
+    console.log("Hola mundo");
+}
+saludar(); // Llamando a la función
+
+function sumar () {
+    return 2 + 2;
+}
+let resultado = sumar(); // Guardando el valor retornado
+console.log("Resultado de la suma:", resultado);
