2ca10df6422261c673a673d85adc0ddd5dbe9295
Esteban Escorcia <estebanfucho98@gmail.com>
2025-12-03 18:56:34 -0800

    Operadores de comparacion

---
+++ 03-operadores/03-comparacion.js
@@ -0,0 +1,11 @@
+let a = 10;
+console.log("Valor de a: " + a);
+console.log("Comparación a == 10: " + (a == 10));   // Igualdad
+console.log("Comparación a === 10: " + (a === "10"));  // Igualdad estricta
+console.log("Comparación a != 5: " + (a != 10));   // Desigualdad
+console.log("Comparación a !== 5: " + (a !== "10"));  // Desigualdad estricta
+
+console.log("Comparación a > 5: " + (a > 5));    // Mayor que
+console.log("Comparación a < 15: " + (a < 5));   // Menor que
+console.log("Comparación a >= 10: " + (a >= 5));  // Mayor o igual que
+console.log("Comparación a <= 20: " + (a <= 5));  // Menor o igual que
