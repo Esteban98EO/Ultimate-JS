4977c17fe65c54bb987835385502a48cb6bc1769
Esteban Escorcia <estebanfucho98@gmail.com>
2025-12-03 18:11:39 -0800

    Operadores aritmeticos

---
+++ 03-operadores/01-aritmeticos.js
@@ -0,0 +1,23 @@
+let a = 5;
+let b = 7;
+
+// console.log("Suma: " + (a + b));          // Suma
+// console.log("Resta: " + (a - b));         // Resta
+// console.log("Multiplicación: " + (a * b)); // Multiplicación
+// console.log("División: " + (a / b));       // División
+// console.log("Módulo: " + (a % b));         // Módulo
+// console.log("Exponenciación: " + (a ** b)); // Exponenciación  o potenciación
+// console.log("Incremento: " + (++a));       // Incremento
+// console.log("Decremento: " + (--b));       // Decremento
+
+console.log("Valor de a: " + a);
+console.log(a++);
+console.log("Valor de a después del post-incremento: " + a);
+console.log(++a);
+console.log("Valor de a después del pre-incremento: " + a);
+
+console.log("Valor de a: " + a);
+console.log(a--);
+console.log("Valor de a después del post-decremento: " + a);
+console.log(--a);
+console.log("Valor de a después del pre-decremento: " + a);
