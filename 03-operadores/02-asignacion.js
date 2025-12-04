214938103ec2114bc906cf45a4a160a451dc0700
Esteban Escorcia <estebanfucho98@gmail.com>
2025-12-03 18:35:41 -0800

    Operadores de asignacion

---
+++ 03-operadores/02-asignacion.js
@@ -0,0 +1,18 @@
+let a = 5;
+a = a + 5; // Asignación con suma
+console.log("Después de a = a + 5: " + a);
+a += 5;    // Asignación con suma abreviada
+console.log("Después de a += 5: " + a);
+
+a += 5; // Asignación con suma abreviada
+console.log("Después de (suma) a += 5: " + a);
+a -= 3; // Asignación con resta abreviada
+console.log("Después de (resta) a -= 3: " + a);
+a *= 2; // Asignación con multiplicación abreviada
+console.log("Después de (multiplicacion) a *= 2: " + a);
+a /= 4; // Asignación con división abreviada
+console.log("Después de (división) a /= 4: " + a);
+a %= 3; // Asignación con módulo abreviada
+console.log("Después de (modulo) a %= 3: " + a);
+a **= 3; // Asignación con exponenciación abreviada
+console.log("Después de (exponente) a **= 3: " + a);
